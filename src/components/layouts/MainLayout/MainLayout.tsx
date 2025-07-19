282fa40fda56f068672028ac0541d62e949d3ee4
HLoc26 <dhl26052004@gmail.com>
2025-07-19 21:02:05 +0700

    refactor: remove redundant console.log

--- src/components/layouts/MainLayout/MainLayout.tsx
+++ src/components/layouts/MainLayout/MainLayout.tsx
@@ -13,8 +13,6 @@ interface MainLayoutProps {
 
 // Main Component
 const MainLayout: React.FC<MainLayoutProps> = ({ currentUser, activeTab, onChangeTab }) => {
-	console.log("Rerender");
-
 	return (
 		<div className="flex flex-col h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
 			<Header currentUser={currentUser} setActiveTab={onChangeTab} />
