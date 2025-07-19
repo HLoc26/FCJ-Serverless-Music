c7c11731241b166ba292ba148efbc57bf228b537
HLoc26 <dhl26052004@gmail.com>
2025-07-19 21:00:05 +0700

    fix: update axios instance configuration

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,10 +1,27 @@
+import { fetchAuthSession } from 'aws-amplify/auth'
 import axios, { type AxiosInstance } from 'axios'
 const axiosInstance: AxiosInstance = axios.create({
     baseURL: "https://por59s0bag.execute-api.us-east-1.amazonaws.com/dev/",
+    // baseURL: "http://localhost:3000/api/",
     headers: {
         "Content-Type": "application/json"
     },
-    withCredentials: true
 })
 
+axiosInstance.interceptors.request.use(
+    async (config) => {
+        try {
+            const session = await fetchAuthSession()
+            const token = session.tokens?.idToken?.toString()
+            config.headers.Authorization = `Bearer ${token}`
+        } catch (error) {
+            console.log('No valid session')
+        }
+
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
 export default axiosInstance
