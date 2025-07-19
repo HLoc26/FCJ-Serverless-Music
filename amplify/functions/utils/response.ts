6e48aae87baf15da583e62ecfcf5520aceb19c2d
HLoc26 <dhl26052004@gmail.com>
2025-07-19 13:09:39 +0700

    feat: Adding CORS headers in response

--- amplify/functions/utils/response.ts
+++ amplify/functions/utils/response.ts
@@ -1,5 +1,14 @@
-export const jsonResponse = (statusCode: number, data: any) => ({
-    statusCode,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data),
-});
+export const jsonResponse = (statusCode: number, data: any) => {
+    const corsHeaders = {
+        'Access-Control-Allow-Origin': 'http://localhost:5173',
+        'Access-Control-Allow-Headers': 'Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token',
+        'Access-Control-Allow-Methods': 'GET,POST,DELETE,OPTIONS',
+        'Access-Control-Allow-Credentials': 'true',
+        'Content-Type': 'application/json'
+    };
+    return {
+        statusCode,
+        headers: corsHeaders,
+        body: JSON.stringify(data),
+    }
+};
