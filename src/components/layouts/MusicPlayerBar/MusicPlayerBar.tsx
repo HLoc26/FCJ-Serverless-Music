8ed67203cb716446ed1bec2d61b531826add5288
HLoc26 <dhl26052004@gmail.com>
2025-07-21 09:38:08 +0700

    feat: use context useMusicPlayerContext

--- src/components/layouts/MusicPlayerBar/MusicPlayerBar.tsx
+++ src/components/layouts/MusicPlayerBar/MusicPlayerBar.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import { useState } from "react";
+import AddToPlaylistModal from "./AddToPlaylistModal";
+import ProgressBar from "./ProgressBar";
+import VolumeControl from "./VolumeControl";
+import PlayerControls from "./PlayerControls";
+import TrackInfo from "./TrackInfo";
 import { useMusicPlayerContext } from "../../../contexts/MusicPlayerContext";
 
-import PlayerControls from "./PlayerControls";
-import ProgressBar from "./ProgressBar";
-import TrackInfo from "./TrackInfo";
-import VolumeControl from "./VolumeControl";
-
-// Music Player Bar Component
 const MusicPlayerBar: React.FC = () => {
+	const [isModalOpen, setModalOpen] = useState(false);
+
 	const {
 		currentTrack, //
 		isPlaying,
@@ -25,12 +26,13 @@ const MusicPlayerBar: React.FC = () => {
 		handleSeek,
 		handleVolumeChange,
 	} = useMusicPlayerContext();
+
 	return (
 		<>
 			{currentTrack.id ? (
 				<div className="bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700 px-6 py-4">
 					<div className="flex items-center justify-between">
-						<TrackInfo track={currentTrack} isLiked={isLiked} onToggleLike={toggleLike} />
+						<TrackInfo track={currentTrack} isLiked={isLiked} onToggleLike={toggleLike} onAddToPlaylist={() => setModalOpen(true)} />
 
 						<div className="flex flex-col items-center gap-2 flex-2 max-w-md mx-8">
 							<PlayerControls
@@ -49,9 +51,10 @@ const MusicPlayerBar: React.FC = () => {
 						<VolumeControl volume={volume} isMuted={isMuted} onVolumeChange={handleVolumeChange} onToggleMute={toggleMute} />
 					</div>
 				</div>
-			) : (
-				<></>
-			)}
+			) : null}
+
+			{/* Modal Component */}
+			{isModalOpen && <AddToPlaylistModal track={currentTrack} onClose={() => setModalOpen(false)} />}
 		</>
 	);
 };
