c6864fd7bbe1be3938ee18f5da292c72c14154a5
HLoc26 <dhl26052004@gmail.com>
2025-08-03 14:09:27 +0700

    feat: add delete endpoint

--- amplify/functions/TrackHandler/handler.ts
+++ amplify/functions/TrackHandler/handler.ts
@@ -3,12 +3,15 @@ import { APIGatewayProxyEvent } from "aws-lambda"
 import { getTrackById } from './services/getTrackById';
 import { getAllTracks } from './services/getAllTracks';
 import { postUploadTrack } from './services/postUploadTrack';
+import { deleteTrackById } from "./services/deleteTrackById";
 
 export const handler = async (event: APIGatewayProxyEvent) => {
     // GET /tracks
     if (event.httpMethod === 'GET' && event.path === '/tracks') return await getAllTracks()
     // GET /tracks/{id}
     else if (event.httpMethod === 'GET' && event.resource === '/tracks/{id}') return await getTrackById(event)
+    // DELETE /tracks/{id}
+    else if (event.httpMethod === 'DELETE' && event.resource === '/tracks/{id}') return await deleteTrackById(event)
     // POST /tracks/upload
     else if (event.httpMethod === 'POST' && event.path === '/tracks/upload') return await postUploadTrack(event)
     // INVALID REQUEST
